ff44de6bc202e8cb2a0c879f1954c7043bd475dc
Muhamad Nur Firdaus Bin Samsudin <nurfirdaus.job@gmail.com>
2024-10-31 17:49:46 +0800

    add log

--- src/main/java/com/mspring/controller/MovieController.java
+++ src/main/java/com/mspring/controller/MovieController.java
@@ -2,6 +2,7 @@ package com.mspring.controller;
 
 import com.mspring.model.Movie;
 import com.mspring.service.MovieService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -12,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/movies")
 public class MovieController {
@@ -23,38 +25,50 @@ public class MovieController {
     public ResponseEntity<Page<Movie>> getAllMovies(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
+        log.info("Request --> All Movie {}", size);
         Page<Movie> movies = movieService.getAllMovies(page, size);
+        log.info("Response --> {}",movies.getContent());
         return ResponseEntity.ok(movies);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        log.info("Request --> Movie id {}", id);
         Optional<Movie> movie = movieService.getMovieById(id);
+        log.info("Response --> {}", movie.orElse(null));
         return movie.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/search")
     public ResponseEntity<List<Movie>> searchMoviesByTitle(@RequestParam String title) {
+        log.info("Request --> Movie title {}", title);
         List<Movie> movies = movieService.searchMoviesByTitle(title);
+        log.info("Response --> {}", movies);
         return ResponseEntity.ok(movies);
     }
 
     @GetMapping("/rating")
     public ResponseEntity<List<Movie>> getMoviesByRating(@RequestParam BigDecimal rating) {
+        log.info("Request --> rating {}", rating);
         List<Movie> movies = movieService.getMoviesByRating(rating);
+        log.info("Response --> {}", movies);
         return ResponseEntity.ok(movies);
     }
 
     @PostMapping
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        log.info("Request --> Save Movie {}", movie);
         Movie createdMovie = movieService.createMovie(movie);
+        log.info("Response --> {}", createdMovie);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdMovie);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails) {
         try {
+            log.info("Request --> update Movie {} {}", id, movieDetails);
             Movie updatedMovie = movieService.updateMovie(id, movieDetails);
+            log.info("Response --> {}", updatedMovie);
             return ResponseEntity.ok(updatedMovie);
         } catch (RuntimeException e) {
             return ResponseEntity.notFound().build();
@@ -64,6 +78,7 @@ public class MovieController {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
         try {
+            log.info("Request --> delete Movie {}", id);
             movieService.deleteMovie(id);
             return ResponseEntity.noContent().build();
         } catch (RuntimeException e) {
