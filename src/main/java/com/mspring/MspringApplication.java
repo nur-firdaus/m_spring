5715342a868c5711a6817f8b6254c8d398ff0477
Muhamad Nur Firdaus Bin Samsudin <nurfirdaus.job@gmail.com>
2024-10-31 15:40:54 +0800

    add logback

---
+++ src/main/java/com/mspring/MspringApplication.java
@@ -0,0 +1,12 @@
+package com.mspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MspringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MspringApplication.class, args);
+    }
+
+}
